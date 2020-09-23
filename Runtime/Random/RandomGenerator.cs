b7c104edd9073d42f81f577bf86b5f2c9373c28e
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-09-24 00:36:39 +0200

    Random added

---
+++ Runtime/Random/RandomGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Juce.Utils.Random
+{
+    public class RandomGenerator
+    {
+        private readonly System.Random random = new System.Random();
+
+        public int GetInt()
+        {
+            return random.Next(int.MinValue, int.MaxValue);
+        }
+
+        public int GetInt(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+        public float GetFloat()
+        {
+            double range = (double)float.MaxValue - (double)float.MinValue;
+            double sample = random.NextDouble();
+            double scaled = (sample * range) + float.MinValue;
+
+            return (float)scaled;
+        }
+
+        public float GetFloat(float min, float max)
+        {
+            float range = min - max;
+            double sample = random.NextDouble();
+            double scaled = (sample * range) + float.MinValue;
+
+            return (float)scaled;
+        }
+    }
+}
