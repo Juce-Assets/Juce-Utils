79a86f488312c05182cc6fb1019df35c7be854b1
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-10-18 19:01:31 +0200

    Logo

---
+++ Runtime/Singletons/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Juce.Utils.Singletons
+{
+    public class Singleton<T> where T : class
+    {
+        private static T singletonInstance;
+
+        public static T Instance
+        {
+            get
+            {
+                if(singletonInstance == null)
+                {
+                    singletonInstance = Activator.CreateInstance<T>();
+                }
+
+                return singletonInstance;
+            }
+        }
+    }
+}
