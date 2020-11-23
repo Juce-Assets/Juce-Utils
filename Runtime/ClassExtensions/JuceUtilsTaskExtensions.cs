e995a6c01039c4395e96d5d64eddbeedf4b1ab03
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-11-23 18:32:15 +0100

    Run async

--- Runtime/ClassExtensions/JuceUtilsTaskExtensions.cs
+++ Runtime/ClassExtensions/JuceUtilsTaskExtensions.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 public static class JuceUtilsTaskExtensions
 {
-    public static async void ExecuteAsync(this Task task, Action onFinish = null)
+    public static async void RunAsync(this Task task, Action onFinish = null)
     {
         await task;
 
